10e22d27d545bf881c7f398605a6d4fa3381da36
eddyw <eddyw.dev@outlook.com>
2017-01-04 08:29:40 +0300

    Config file is simplified. 'distDir' is the same in production and development mode.

--- src/server/config/server.js
+++ src/server/config/server.js
@@ -1,16 +1,9 @@
-import path from 'path';
 import webpackConfig from '../../../webpack.client';
-
-const NODE_ENV = process.env.NODE_ENV === 'production' ?
-  'production' :
-  'development';
 
 const config = {
   port: process.env.PORT || 8080,
   host: process.env.IP || 'localhost',
-  distDir: NODE_ENV === 'production' ?
-    './build/dist/' : // server is in build/server.bundle.js
-    path.resolve(__dirname, '../../build/dist/'), // server is in src/server/index.jsx
+  distDir: './build/dist/',
   publicPath: webpackConfig.output.publicPath,
   webpackConfig,
 };
