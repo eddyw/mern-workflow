2be4fcd1ef4530b0b9a79fb1a360a9a80b0ca9ec
eddyw <eddyw.dev@outlook.com>
2017-01-04 08:28:37 +0300

    Add 'maximum-scale' to viewport. Server-Side-Rendering is only done in 'production' because of hot-reloading.

--- src/server/view/home.jsx
+++ src/server/view/home.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { renderToString } from 'react-dom/server';
 
+const NODE_ENV = process.env.NODE_ENV;
+
 class HelloMessage extends React.PureComponent {
   static defaultProps = {
     Application: <p>Missing Application :(</p>,
@@ -24,12 +26,12 @@ class HelloMessage extends React.PureComponent {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <title>{payload.title}</title>
           <meta name="description" content={payload.description} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
           <link rel="stylesheet" href={`/static/css/${applicationName.toLowerCase()}.css`} />{/* Application CSS */}
         </head>
         <body>
-          <h3>{applicationName}</h3>
-          <div id="app-body" dangerouslySetInnerHTML={{ __html: renderToString(<Application />) }} /> {/* Server-Side-Rendering of Application */}
+          {/* <h3>{applicationName}</h3> */}
+          <div id="app-body" dangerouslySetInnerHTML={{ __html: NODE_ENV === 'production' ? renderToString(<Application />) : '' }} /> {/* Server-Side-Rendering of Application */}
           <script dangerouslySetInnerHTML={{ __html: `window.PRELOADED_STATE=${JSON.stringify(preloadedState)};` }} /> {/* Preloaded State of Application */}
           <script src="/static/commons.js" /> {/* Just common JS */}
           <script src={`/static/${applicationName.toLowerCase()}.js`} /> {/* React Application */}
