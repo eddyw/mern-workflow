68182c7cb0e19105d6f070332236a50cc31b333d
eddyw <eddyw.dev@outlook.com>
2017-01-07 06:46:15 +0300

    Fixes and adds comments to the sample app.

--- src/application/HelloWorld/index.jsx
+++ src/application/HelloWorld/index.jsx
@@ -1,53 +1,79 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  Button,
-  Container,
-  Divider,
-  Icon,
-  Menu,
-} from 'semantic-ui-react';
-
-// This file is required by the server for server-side-rendering,
-// so require only when env var is set to "IN_BROWSER".
-// IN_BROWSER is set by webpack to true when transpiling and other tasks.
-if (process.env.IN_BROWSER) require('../../lib/semantic/dist/semantic.css');
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory, RouterContext, IndexRoute } from 'react-router';
+import { createStore, combineReducers, compose } from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import Main from './view/Main';
+import Home from './view/Home';
+import About from './view/About';
 
 // Hot Reload!
 if (module.hot) module.hot.accept();
 
+// This file is required by the server for server-side-rendering,
+// This line is ignored by the server (webpack.IgnorePlugin) but not by the client.
+if (process.env.BROWSER === true) require('../../lib/semantic/dist/semantic.css');
+
+// Enable ReduxDevTools (if installed) only in development and only in client.
+const ReduxDevTools = (
+  process.env.BROWSER === true &&
+  process.env.NODE_ENV === 'development' ? window.devToolsExtension : false
+);
+
+// Store
+export const store = createStore(
+  combineReducers({
+    routing: routerReducer,
+  }),
+  process.env.BROWSER ? window.PRELOADED_STATE : {},
+  compose(ReduxDevTools ? ReduxDevTools() : undefined),
+);
+
+// Routes
+export const Routes = (
+  <Router>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Home} />
+      <Route path="/about" component={About} />
+    </Route>
+  </Router>
+);
+
 // The Application itself.
 class Application extends React.Component {
-  // Load the default state of the application.
-  // PRELOADED_STATE is set by the server. (server-side-rendering)
-  static preloadedState = Object.assign({
-    other: 1,
-  },
-    process.env.IN_BROWSER ? window.PRELOADED_STATE : {},
-  );
+  // The name of the application must coincide with the app's folder name,
+  // For instance, if src/application/[AppName], then name='[AppName]'.
+  // The structure of the generated app is the folloing (always lowercase):
+  //    build/[appname].js
+  //    build/css/[appname].css
+  static name = 'HelloWorld';
+  // This variable is set by the server when matching the url with the routes.
+  static routerProps = {};
   render() {
+    // Client-Side-Rendering
+    if (process.env.BROWSER) {
+      store.dispatch({ type: 'PRELOAD_STATE' });
+      return (
+        <Provider store={store}>
+          <Router history={syncHistoryWithStore(browserHistory, store)}>
+            {Routes}
+          </Router>
+        </Provider>
+      );
+    }
+    // Server-Side-Rendering
     return (
-      <div>{/* Provider */}
-        <Menu attached stackable inverted>
-          <Container>
-            <Menu.Item active onClick={() => {}}>
-              <Icon loading inverted color="teal" name="circle outline" size="large" />
-              Home
-            </Menu.Item>
-          </Container>
-        </Menu>
-        <Divider clearing hidden />
-        <Container>
-          {JSON.stringify(Application.preloadedState)}
-        </Container>
-      </div>
+      <Provider store={store}>
+        <RouterContext {...this.props.routerProps} />
+      </Provider>
     );
   }
 }
 
 // This file is required by the server for server-side-rendering,
 // render only when running on the client-side.
-if (process.env.IN_BROWSER) {
+if (process.env.BROWSER) {
   render(
     <Application />,
     document.getElementById('app-body'),
